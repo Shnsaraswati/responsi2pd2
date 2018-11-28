2ac1dc32dcb77174a642c9038650db49d5d2a952
Shania <shaniasaraswati98@gmail.com>
2018-11-28 20:56:30 +0700

    Updating Database Project

--- responsipd2.sql
+++ responsipd2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 22, 2018 at 02:07 AM
+-- Generation Time: Nov 28, 2018 at 12:54 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -25,44 +25,16 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mahasiswa`
---
-
-CREATE TABLE `mahasiswa` (
-  `nrp` varchar(10) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `ttl` varchar(30) NOT NULL,
-  `alamat` varchar(60) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `mahasiswa`
---
-
-INSERT INTO `mahasiswa` (`nrp`, `nama`, `ttl`, `alamat`) VALUES
-('171111011', 'shania saraswati', 'malang 24 oktober 1998', 'sempalwadak'),
-('171111040', 'risky feryansyah pribadi', 'malang 7 november 1998', 'gadang');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `tabungan`
 --
 
 CREATE TABLE `tabungan` (
   `nrp` varchar(10) NOT NULL,
-  `simpanan` int(100) NOT NULL,
-  `penariakn` int(100) NOT NULL,
-  `saldo` int(100) NOT NULL
+  `uang_masuk` int(50) DEFAULT NULL,
+  `uang_keluar` int(50) DEFAULT NULL,
+  `tanggal` date NOT NULL,
+  `saldo` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tabungan`
---
-
-INSERT INTO `tabungan` (`nrp`, `simpanan`, `penariakn`, `saldo`) VALUES
-('171111011', 300000, 0, 300000),
-('171111040', 500000, 100000, 400000);
 
 -- --------------------------------------------------------
 
@@ -71,29 +43,27 @@ INSERT INTO `tabungan` (`nrp`, `simpanan`, `penariakn`, `saldo`) VALUES
 --
 
 CREATE TABLE `user` (
-  `nrp` varchar(10) DEFAULT NULL,
-  `nama` varchar(50) NOT NULL,
-  `ttl` varchar(30) NOT NULL,
-  `alamat` varchar(60) DEFAULT NULL,
-  `password` varchar(10) NOT NULL
+  `nrp` varchar(10) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `jk` char(1) NOT NULL,
+  `telp` int(12) NOT NULL,
+  `alamat` text NOT NULL,
+  `password` varchar(5) NOT NULL,
+  `ttl` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`nrp`, `nama`, `ttl`, `alamat`, `password`) VALUES
-('171111011', 'shania saraswati', 'Malang 24 oktober 1998', 'sempalwadak', 'a123');
+INSERT INTO `user` (`nrp`, `nama`, `jk`, `telp`, `alamat`, `password`, `ttl`) VALUES
+('161111001', 'Aditya  Fais Rofi', 'L', 85143, 'Bululawang', 'c123', 'Malang 28 November 1997'),
+('171111011', 'shania saraswati', 'P', 821789156, 'Sempalwadak', 'a123', 'Malang 24 oktober 1998'),
+('171111040', 'Risky Feryansyah P', 'L', 8383, 'gadang gang 5c', 'b123', 'Malang 7 November 1998');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `mahasiswa`
---
-ALTER TABLE `mahasiswa`
-  ADD PRIMARY KEY (`nrp`);
 
 --
 -- Indexes for table `tabungan`
@@ -105,7 +75,7 @@ ALTER TABLE `tabungan`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD KEY `nrp` (`nrp`);
+  ADD PRIMARY KEY (`nrp`);
 
 --
 -- Constraints for dumped tables
@@ -115,13 +85,7 @@ ALTER TABLE `user`
 -- Constraints for table `tabungan`
 --
 ALTER TABLE `tabungan`
-  ADD CONSTRAINT `tabungan_ibfk_1` FOREIGN KEY (`nrp`) REFERENCES `mahasiswa` (`nrp`);
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `nrp` FOREIGN KEY (`nrp`) REFERENCES `mahasiswa` (`nrp`);
+  ADD CONSTRAINT `tabungan_ibfk_1` FOREIGN KEY (`nrp`) REFERENCES `user` (`nrp`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
